2e8054c80af6c44413577a13cc3e094f1555be94
Raitig sarkar <166475951+raitig@users.noreply.github.com>
2025-05-30 20:00:49 +0530

    Add files via upload

---
+++ DiscordBOT/main.py
@@ -0,0 +1,87 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+intents = discord.Intents.default()
+intents.message_content = True 
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+secret_role = "Reporters"
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+    
+@bot.event
+async def on_member_join(member):
+   await member.send(f"Welcome to the server {member.name}") 
+   
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - don't use that word!")
+        
+    await bot.process_commands(message)
+    
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+    
+@bot.command()
+async def assign(ctx:commands.Context):
+    role = discord.utils.get(ctx.guild.roles, name=secret_role)
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} is now assigned to {secret_role}")
+    else:
+        await ctx.send("Role doesn't exist")    
+        
+@bot.command()
+async def remove(ctx:commands.context):
+    role = discord.utils.get(ctx.guild.roles, name=secret_role)
+    if role:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f"{ctx.author.mention} had had the {secret_role} remove ")
+    else:
+        await ctx.send("Role doesn't exist")  
+        
+@bot.command()
+async def dm(ctx, *, msg):
+    await ctx.author.send(f"you said {msg}")
+    
+@bot.command()
+async def reply(ctx):
+    await ctx.reply("This is a reply to your message !")
+    
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New poll", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+        
+@bot.command()
+@commands.has_role(secret_role)
+async def secret(ctx):
+    await ctx.send("Welcome to the club !")  
+        
+        
+@secret.error
+async def secrect_error(ctx,error):
+    if isinstance(error, commands.MissingRole):
+     await ctx.send("you do not have permission to do that !")
+
+bot.run(token)
