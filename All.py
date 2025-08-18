a82463bcb29fd181d760ad9eac39b144565615bf
Raitig sarkar <166475951+raitig@users.noreply.github.com>
2025-08-19 00:20:30 +0530

    All.py

--- All.py
+++ All.py
@@ -45,3 +45,65 @@ print(f"number of square {num} is {square}")
 # Write a python program to display a user entered name followed by good afternoon using input() function 
 Who = input("Enter your name : ")
 print("Good morning,", Who )
+
+
+# Write a program to fill in a letter template given below with name and date 
+letter = """ Dear <|Name|>,
+         you are selected ! 
+        <|Date|> """
+Name = input("Enter your name:")
+Date = input("Enter the date:")
+letter = letter.replace("<|Name|>",Name)
+letter = letter. replace("<|Date|>", Date)
+print(letter)
+
+
+# Write a program to detect double space in a string
+a = input("Enter your string :")
+if "  " in a :
+    print("Double space is detected....!")
+else :
+    print(" Dosen't detected the Double space.....!")
+
+
+# Replace the double space from problem 3 with single space 
+a = input("Enter your string :")
+b = a.replace(" " , " ")
+print("After", a)
+print("Before",b)
+
+
+# Write a program to sort the list 
+list = [2,6,5,1,8,90,34,32,67]
+list.sort()
+print(list)
+
+
+# Write a program to reverse the list
+list = [2,6,5,1,8,90,34,32,67]
+list.reverse()
+print(list)
+
+
+# Write a program to append the list 
+list = [2,6,5,1,8,90,34,32,67]
+list.append(100)
+print(list)
+
+
+# Write a program to insert the list 
+list = [2,6,5,1,8,90,34,32,67]
+list.insert(7,3)
+print(list)
+
+
+# Write a program to pop the list
+list = [2,6,5,1,8,90,34,32,67]
+list.pop()
+print(list)
+
+
+# Write a program to remove the list 
+list = [2,6,5,1,8,90,34,32,67]
+list.remove(90)
+print(list)
